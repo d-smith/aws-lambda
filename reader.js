76444b25e1eaa54f418f43ad80ca3f4eb21d6f47
d-smith <doug.smith.mail@gmail.com>
2014-11-22 06:46:32 -0700

    added reader

---
+++ reader.js
@@ -0,0 +1,71 @@
+"use strict";
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+AWS.config.loadFromPath('../config.json');
+
+
+var kinesis = new  AWS.Kinesis();
+
+var readStream = function(shardItorId) {
+
+	var i, rsParams, rec;
+	rsParams = {
+  	ShardIterator: shardItorId
+	};
+
+  kinesis.getRecords(rsParams, function(err, data) {
+
+  	if (err) {
+  		console.log("error on getRecords call");
+  		console.log(err, err.stack);
+  	} else {
+  		for (i = 0; i < data.Records.length; i = i + 1) {
+    			rec = data.Records[i];
+    			console.log(rec.Data.toString());
+  		}
+    	readStream(data.NextShardIterator);
+  	}
+  });
+
+};
+
+var processShard = function(streamName, shardId) {
+	var params = {
+		ShardId: shardId,
+		ShardIteratorType: 'TRIM_HORIZON',
+		StreamName: streamName
+	};
+
+	kinesis.getShardIterator(params, function(err,data) {
+		if(err) {
+			console.log(err, err.stack);
+		} else {
+			readStream(data.ShardIterator);
+		}
+	});
+};
+
+
+
+var processStream = function(streamName) {
+	var params = {
+		StreamName: streamName
+	};
+
+	kinesis.describeStream(params, function(err, data) {
+		var i, shards;
+		if (err) {
+			console.log(err, err.stack);
+			process.exit(1);
+		} else {
+			shards = data.StreamDescription.Shards;
+			if(shards.length > 0) {
+				for(i = 0; i < shards.length; i = i + 1) {
+				processShard(streamName, shards[i].ShardId);
+			}
+		}
+		}
+	});
+};
+
+processStream('loggingStream2');
