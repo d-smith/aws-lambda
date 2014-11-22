3552d4c24e5111757b765d60936d527fabe9010f
d-smith <doug.smith.mail@gmail.com>
2014-11-22 06:43:35 -0700

    Added addEventStream call

--- setup-kinesis-event.js
+++ setup-kinesis-event.js
@@ -5,6 +5,15 @@ AWS.config.update({region: 'us-east-1'});
 AWS.config.loadFromPath('../config.json');
 
 var kinesis = new AWS.Kinesis();
+var lambda = new AWS.Lambda();
+
+lambda.listFunctions({}, function(err, data) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
 
 
 kinesis.describeStream({
@@ -17,8 +26,23 @@ kinesis.describeStream({
     } else {
       var streamARN = data.StreamDescription.StreamARN;
       console.log(streamARN);
+
+      var lambdaEventSourceParams = {
+        EventSource: streamARN,
+        FunctionName: 'streamReader',
+        Role: 'arn:aws:iam::930295567417:role/son_of_exec_role',
+        Parameters : {
+          InitialPositionInStream: 'TRIM_HORIZON'
+        }
+      };
+
+      lambda.addEventSource(lambdaEventSourceParams, function(err, data) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log(data.Status);
+        }
+      });
     }
   }
 );
-
-var lambda = new AWS.Lambda();
