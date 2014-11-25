4d5217250c1f799b598d5fe31b95091af9399f6b
d-smith <doug.smith.mail@gmail.com>
2014-11-25 09:30:34 -0700

    Updated setup-details to use proxy config if indicated

--- setup-detail.js
+++ setup-detail.js
@@ -4,20 +4,23 @@ var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 AWS.config.loadFromPath('../config.json');
 
+var httpProxy = process.env.http_proxy;
+if(httpProxy !== null) {
+  console.log('setting http proxy using process.env.http_proxy');
+  AWS.config.update({
+    httpOptions: {
+      proxy: httpProxy
+    }
+  });
+} else {
+  console.log("No proxy settings found");
+}
+
 var lambda = new AWS.Lambda();
-
-lambda.getFunctionConfiguration({FunctionName: 'streamReader'}, function(err, data) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("function configuration");
-    console.log(data);
-  }
-});
-
 var kinesis = new AWS.Kinesis();
 
-kinesis.describeStream({
+var listEventSourcesForStream = function() {
+  kinesis.describeStream({
     StreamName : "loggingStream2",
     Limit: 1
   },
@@ -26,6 +29,7 @@ kinesis.describeStream({
       console.log(err);
     } else {
       var streamARN = data.StreamDescription.StreamARN;
+      console.log('list event sources for %s', streamARN);
 
       lambda.listEventSources({
         EventSourceArn: streamARN,
@@ -33,9 +37,26 @@ kinesis.describeStream({
       }, function(err, data) {
         if(err) {
           console.log(err);
+          process.exit(1);
         } else {
           console.log(data);
         }
       });
     }
-});
+  });
+};
+
+
+
+var describeFunctionConfig = function() {
+  lambda.getFunctionConfiguration({FunctionName: 'streamReader'}, function(err, data) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("function configuration");
+      console.log(data);
+    }
+  });
+};
+
+listEventSourcesForStream();
